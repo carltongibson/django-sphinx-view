38ba313ba01882614ec959ccd3988551abd9e948
Carlton Gibson <carlton.gibson@noumenal.es>
2021-05-15 20:36:21 +0200

    Added initial DocumentationView.

--- README.md
+++ README.md
@@ -2,3 +2,37 @@
 Serve your Sphinx docs with Django.
 
 Current status: pre-alpha. You're far too early. 😀
+
+## Basic Usage
+
+Build your Sphinx docs with the `JSONBuilder`, using `make json`.  
+
+Provide a `sphinx_view/page.html` template. This package will ship one in a later 
+version.
+
+Route a `DocumentationView` with a `Path` to the output json. 
+
+```python
+from pathlib import Path
+
+from django.urls import path
+from sphinx_view import DocumentationView
+
+urlpatterns = [
+    # ...
+    path(
+        "docs<path:path>",
+        DocumentationView.as_view(
+            json_build_dir=Path('/path/to/output/json')
+        ),
+        name="documentation",
+    ),
+    # ...
+]
+```
+
+Enjoy. 
+
+You can subclass `DocumentationView` to require authentication and so add 
+access control, and so on. You can customise the template to add other dynamic 
+elements.  
---
+++ sphinx_view/views.py
@@ -0,0 +1,34 @@
+import json
+from pathlib import Path
+
+from django.http import Http404
+from django.views.generic.base import TemplateView
+
+
+class DocumentationView(TemplateView):
+    json_build_dir: Path = None
+
+    def get_doc_json(self):
+        path: str = self.kwargs["path"]
+        print(path)
+        if not path.endswith("/"):
+            raise Http404
+
+        base = Path(self.json_build_dir)
+        options = [
+            Path(path[1:] + "index.fjson"),
+            Path(path[1:-1] + ".fjson"),
+        ]
+        for o in options:
+            candidate = base / o
+            print(candidate)
+            if candidate.exists():
+                with candidate.open("r") as f:
+                    return json.load(f)
+
+        raise Http404
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["doc"] = self.get_doc_json()
+        return context
