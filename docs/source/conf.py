87cb9a2667d5c52ea4a6698360c2b24d711a19a2
Carlton Gibson <carlton.gibson@noumenal.es>
2021-05-29 09:25:27 +0200

    Added Myst-parser.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -28,6 +28,7 @@ author = 'Carlton Gibson'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
+    "myst_parser",
 ]
 
 # Add any paths that contain templates here, relative to this directory.
